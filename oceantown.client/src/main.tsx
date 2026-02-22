b7045a8103dcde934e415b431bdc43430c9fb4ea
Chase Conaway <chase.conaway@wsu.edu>
2026-02-21 23:50:29 -0800

    messed it up

--- oceantown.client/src/main.tsx
+++ oceantown.client/src/main.tsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css'; 
 import App from './App.tsx';
-import FunctionDefinitionPage from './Simulations/FunctionDefinitionPage';
-
-const url = window.location.pathname;
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        {url === '/function-definition' ? <FunctionDefinitionPage /> : <App />}
+        <App />
     </StrictMode>
 );
